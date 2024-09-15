4a5e8dc2cf66fb05775950f6677fafae2c5281fd
juviZ138 <116240388+juviZ138@users.noreply.github.com>
2024-09-15 20:00:09 +0700

    End of section 6

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, output,  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -12,6 +13,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent {
   private accountService = inject(AccountService);
+  private toaster = inject(ToastrService);
   cancelRegister = output<boolean>();
   model: any = {};
 
@@ -19,9 +21,10 @@ export class RegisterComponent {
     this.accountService.register(this.model).subscribe({
       next: response => {
         console.log(response);
+        
         this.cancel(); 
       },
-      error: error => console.log(error)
+      error: error => this.toaster.error(error.error)
     });
   }
 
