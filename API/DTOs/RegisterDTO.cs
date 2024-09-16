d6282621496d674772ae4d54e6ce734d828d3a24
juviZ138 <116240388+juviZ138@users.noreply.github.com>
2024-09-16 23:43:41 +0700

    end of section 7

--- API/DTOs/RegisterDTO.cs
+++ API/DTOs/RegisterDTO.cs
@@ -6,7 +6,8 @@ namespace API.DTOs;
 public class RegisterDTO
 {
     [Required]
-    public required string Username { get; set; }
+    public  string Username { get; set; } = String.Empty;
     [Required]
-    public required string Password { get; set; }
+    [StringLength(8,  MinimumLength = 4)]
+    public  string Password { get; set; } = String.Empty;
 }
