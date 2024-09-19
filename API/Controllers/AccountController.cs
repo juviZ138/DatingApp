f02643f595619fb0d6e04c5169912514d4941af6
juviZ138 <116240388+juviZ138@users.noreply.github.com>
2024-09-19 21:29:43 +0700

    End of section 8

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -18,23 +18,26 @@ public class AccountController(DataContext context, ITokenService tokenService) 
     {
         if(await UserExists(registerDTO.Username)) return BadRequest("Username is taken");
 
-        using var hmac = new HMACSHA512();
+        return Ok();
+
+        // using var hmac = new HMACSHA512();
         
-        var user = new AppUser
-        {
-            UserName = registerDTO.Username.ToLower(),
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
-            PasswordSalt = hmac.Key  
-        };
+        // var user = new AppUser
+        // {
+        //     UserName = registerDTO.Username.ToLower(),
+        //     PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+        //     PasswordSalt = hmac.Key,
 
-        context.Users.Add(user);
-        await context.SaveChangesAsync();
+        // };
 
-        return new UserDTO
-        {
-            Username = user.UserName,
-            Token = tokenService.CreateToken(user)
-        };
+        // context.Users.Add(user);
+        // await context.SaveChangesAsync();
+
+        // return new UserDTO
+        // {
+        //     Username = user.UserName,
+        //     Token = tokenService.CreateToken(user)
+        // };
 
     }
 
