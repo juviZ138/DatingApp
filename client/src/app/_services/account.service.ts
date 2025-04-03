4b61d7bfc8205a66d437619cd41bde77b7768ce9
juviZ138 <quachlydientam@gmail.com>
2025-04-03 15:53:41 +0700

    End of section 14

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -3,31 +3,33 @@ import { inject, Injectable, signal } from '@angular/core';
 import { User } from '../_models/user';
 import { map } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { LikesService } from './likes.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
   private http = inject(HttpClient);
+  private likeService = inject(LikesService);
   baseUrl = environment.apiUrl;
-  currentUser = signal<User | null>(null)
-  
-  login(model: any){
-    return this.http.post<User>(this.baseUrl +  'account/login', model).pipe(
-      map( user => {
-        // if has user 
-        if(user){
+  currentUser = signal<User | null>(null);
+
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((user) => {
+        // if has user
+        if (user) {
           this.setCurrentUser(user);
         }
       })
     );
   }
 
-  register(model: any){
-    return this.http.post<User>(this.baseUrl +  'account/register', model).pipe(
-      map( user => {
-        // if has user 
-        if(user){
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map((user) => {
+        // if has user
+        if (user) {
           this.setCurrentUser(user);
         }
         return user;
@@ -35,15 +37,14 @@ export class AccountService {
     );
   }
 
-  setCurrentUser(user: User){
+  setCurrentUser(user: User) {
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUser.set(user);
+    this.likeService.getLikeIds();
   }
-
 
   logout() {
     localStorage.removeItem('user');
     this.currentUser.set(null);
   }
-  
 }
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavComponent } from "./nav/nav.component";
+import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
-import { HomeComponent } from "./home/home.component";
+import { HomeComponent } from './home/home.component';
 import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
@@ -10,11 +10,10 @@ import { NgxSpinnerComponent } from 'ngx-spinner';
   standalone: true,
   imports: [RouterOutlet, NavComponent, HomeComponent, NgxSpinnerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-
-export class AppComponent implements OnInit{
-  private accountService = inject(AccountService);  
+export class AppComponent implements OnInit {
+  private accountService = inject(AccountService);
 
   ngOnInit(): void {
     this.setCurrentUser();
@@ -22,9 +21,8 @@ export class AppComponent implements OnInit{
 
   setCurrentUser() {
     const userString = localStorage.getItem('user');
-    if(!userString) return;
+    if (!userString) return;
     const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
+    this.accountService.setCurrentUser(user);
   }
-
 }
